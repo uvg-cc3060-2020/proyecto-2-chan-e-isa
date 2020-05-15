57c9ef0239e9cae52db252100b8712663f3cfecb
Michael Chan <mschangtaitai@gmail.com>
2020-05-14 23:29:50 -0600

    prototipo movimiento

---
+++ ElementalGauntlet/Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        Vector3 vectorMovement = new Vector3(xInput, 0, 0);
+
+        rigidbody.AddForce(vectorMovement * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 vectorJump = new Vector3(0, jumpForce, 0);
+            rigidbody.AddForce(vectorJump * jumpForce * Time.deltaTime, ForceMode.Impulse);
+        }
+    }
+}
